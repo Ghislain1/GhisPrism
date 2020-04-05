a861303327daabed15f4d35a13bf25d73521ad86
Ghis <Gzeleu@protonmail.com>
2020-04-05 10:02:49 +0200

    Wrong wait to make rollback or revert usign GIT

--- CleanAll.ps1
+++ CleanAll.ps1
@@ -1,7 +1,12 @@
 $binAndObjFolder = Get-ChildItem .\ -include bin, obj -Recurse 
 Write-Output $binAndObjFolder
 
-$binAndObjFolder | ForEach-Object ($_) { remove-item $_.fullname -Force -Recurse  -verbose }
+$count = 0
+$binAndObjFolder | ForEach-Object ($_) { 
+    count++;
+    Start-Sleep -Second 3
+    Write-Output $count
+    remove-item $_.fullname -Force -Recurse  -verbose }
 
 ForEach ($number in 1..10 ) {
     if ($number % 7 -eq 0) {
